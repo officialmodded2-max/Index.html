b7d345994cbe1c32dbe0c19130c7fbc3943e5d5b
officialmodded2-max <officialmodded2@gmail.com>
2026-01-21 20:14:59 +0800

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-function copyScript(id) {
-    const text = document.getElementById(id).innerText;
-
-    navigator.clipboard.writeText(text).then(() => {
-        alert("Script copied to clipboard!");
-    }).catch(() => {
-        alert("Copy failed.");
-    });
-}
